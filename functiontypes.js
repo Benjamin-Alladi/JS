c29c35da464717ab18d44b418e5bd8c24d0258d9
Benjamin Alladi <147035130+Benjamin-Alladi@users.noreply.github.com>
2023-12-11 17:06:53 +0530

    HOF

--- functiontypes.js
+++ functiontypes.js
@@ -1,11 +1,14 @@
-// Function Declaration:
+// Types of Functions in JS:
+// 1.Function Declaration  2.Function EXpression(Named and Ananymous)  3.Arrow Functions  4.Call-back Functions  5.IIFE  6.Higher-Order Function
+
+// 1.Function Declaration:
 function f() {
     console.log("This is Function Declaration");
 }
 f();
 
 
-// Function Expression:
+// 2.Function Expression:
 let func= function myFunc()
 {
     console.log("This is Function Expression");
@@ -13,16 +16,28 @@ let func= function myFunc()
 func();
 
 
-// Arrow Function:
-let x= () =>  console.log("This is Arrow Function");
+// 3.Arrow Function:
+let x= ()=>console.log("This is Arrow Function");
 x();
 
 
 
-// iife: Immediately Invoking Function Expression:
+// 4. IIFE: Immediately Invoking Function Expression:
 (
     function()
     {
         console.log("This is iife");
     }
 )();
+
+
+// 6. Higher-Order Function:
+// The Function that takes the other Function as a parameter is called as "Higher-Order Function".
+// There are various built in HOFs, and some of the most common ones are map(), filter() and reduce().
+
+function outer(inner)   //Here, outer if HOF and inner is Call-back function.
+{
+    inner();
+}
+
+outer(()=>console.log("Hello"));
