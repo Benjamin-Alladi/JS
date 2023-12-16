beb03488e252b0670ffa170f4ae85f6554ecb4fe
Benjamin Alladi <147035130+Benjamin-Alladi@users.noreply.github.com>
2023-12-16 12:45:42 +0530

    function scope

---
+++ functionscope.js
@@ -0,0 +1,31 @@
+// function outer()
+// {
+//     var b=10;
+//     function inner()
+//     {
+//         var e=300;
+//         let g;
+//         console.log("Inside inner: "+x+" "+a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g);
+//         console.log(g);
+//         // g=500;
+//         var f=400;
+//         // let g=500;
+//     }
+//     var c=100;
+//     inner();
+
+//     var d=200;
+// }
+
+// var x=1;
+// outer();
+// var a=10;
+
+var a;
+a=1;
+
+let x;
+console.log(a+" "+b+" "+x);
+x=10;
+
+var b=100;
