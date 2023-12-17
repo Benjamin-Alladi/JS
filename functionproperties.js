302beb4b73f6d5e9fab205ac6ba5a0366bfac9f0
Benjamin Alladi <147035130+Benjamin-Alladi@users.noreply.github.com>
2023-12-17 17:39:13 +0530

    Function Properties

---
+++ functionproperties.js
@@ -0,0 +1,27 @@
+// FUNCTION METHODS:
+// 1.func.name   2.func.displayName   3. func.arguments   4. func.length
+
+function func(a,b)
+{
+    console.log("1. Arguments: "+ JSON.stringify(func.arguments));
+    console.log("2. Length: "+ func.length);
+}
+
+func(10,20);
+
+console.log("3. Name: "+ func.name);
+
+func.displayName = "MyFunction";
+
+console.log("4. DisplayName: "+ func.displayName);
+
+console.log("Name is: "+ func.name);
+console.log(func);
+
+console.log("toString():\n"+ func.toString());
+
+
+
+
+
+
